ceba8c3bcf2fafad8ed3059254d8e804f0eac7c9
waleedqu <waleedq93@msn.com>
2023-04-25 22:16:56 -0700

    conditionals to tell weather

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+const temperature = -1;
+
+if (temperature < 0) {
+  console.log("make sure you wear a scarf");
+} else if (temperature < 15) {
+  console.log("short sleeves wont cut it");
+} else {
+  console.log("short sleeves are fine");
+}
+
+console.log("now youre ready to go outside");
