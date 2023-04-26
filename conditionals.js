baf863d3812886068e95a8a8a2dc9ed173d0a6e6
waleedqu <waleedq93@msn.com>
2023-04-25 22:28:16 -0700

    more examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,4 @@
+
 const temperature = -1;
 
 if (temperature < 0) {
@@ -9,3 +10,22 @@ if (temperature < 0) {
 }
 
 console.log("now youre ready to go outside");
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("you are elgible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("maybe going outside isnt a great idea");
+}
+
+
+const raining = false;
+if (!raining) {
+  console.log("leave your umbrella at home");
+}
